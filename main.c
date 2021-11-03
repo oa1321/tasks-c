887b4089e47b6bffec28c1bf28c37df3a155fe06
oa1321 <74298257+oa1321@users.noreply.github.com>
2021-11-03 13:33:42 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -9,41 +9,41 @@ int main(){
    printf("The Armstrong numbers are:");
    for(int i = a; i < b; i+=1){
    		if(isArmstrong(i)){
-   			printf("%d ",i);
+   			printf(" %d",i);
    		}
    }
    if(isArmstrong(b)){
-   			printf("%d",b);
+   			printf(" %d",b);
    		}
    printf("\n");
    printf("The Palindromes are:");
    for(int i = a; i < b; i+=1){
    		if(isPalindrome(i)){
-   			printf("%d ",i);
+   			printf(" %d",i);
    		}
    }
    if(isPalindrome(b)){
-   			printf("%d",b);
+   			printf(" %d",b);
    		}
    printf("\n");
    printf("The Prime numbers are:");
    for(int i = a; i < b; i+=1){
    		if(isPrime(i)){
-   			printf("%d ",i);
+   			printf(" %d",i);
    		}
    }
    if(isPrime(b)){
-   			printf("%d",b);
+   			printf(" %d",b);
    		}
    printf("\n");
    printf("The Strong numbers are:");
    for(int i = a; i < b; i+=1){
    		if(isStrong(i)){
-   			printf("%d ",i);
+   			printf(" %d",i);
    		}
    }
    if(isStrong(b)){
-   			printf("%d",b);
+   			printf(" %d",b);
    		}
    printf("\n");
    return 0;
